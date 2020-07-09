94ad8f565718d037516d0fd08fed51074e44f254
Stanislav <lemeshaevs@gmail.com>
2020-07-09 23:36:53 +0300

    cast type

---
+++ CastType.playground/Contents.swift
@@ -0,0 +1,10 @@
+// Приведение типов
+var intNumber: Int = 28
+var doubleNumber: Double = 18.1
+
+var sum: Int = intNumber + Int(doubleNumber)
+var div: Double = Double(intNumber) - doubleNumber
+print(div)
+
+var lineString: String = "sum = " + String(sum)
+print(lineString)
