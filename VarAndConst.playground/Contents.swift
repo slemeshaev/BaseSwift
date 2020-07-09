b9c67d6d6bbd5c8b94b2dde5b2070b183e4fbb5a
Stanislav <lemeshaevs@gmail.com>
2020-07-09 15:22:00 +0300

    Pi

--- VarAndConst.playground/Contents.swift
+++ VarAndConst.playground/Contents.swift
@@ -7,3 +7,12 @@ number = 18
 
 // let имяКонстанты = 3.14
 let Pi = 3.14
+
+print("My Program")
+print(Pi)
+
+// 1-й способ
+print("Константа Pi равна", Pi)
+
+// 2-й способ
+print("Константа Pi равна \(Pi)")
